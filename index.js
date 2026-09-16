76537ce6432d682c92c7b44a86439385dcde8efb
MartinCsupor <csupormartin@turr.hu>
2026-09-16 11:27:20 +0200

    4. Hibakezelés

--- index.js
+++ index.js
@@ -52,7 +52,19 @@ let output = document.getElementById("output");
 
 function honapadatai(){
     let honapinput = document.getElementById("honapInput").value;
-    let adatok = honapok[honapinput -1]
 
-    output.textContent = `${honapinput} Hónap: ${adatok.nev} Évszak: ${adatok.evszak} Napok száma: ${adatok.napok}`;
+    let adatok;
+    try {
+        if (honapinput < 1 || honapinput > 12) {
+            throw new Error("A hónapszámnak 1 és 12 közé kell esnie!");
+        }
+        else if (isNaN(honapinput)) {
+            throw new Error("A hónapszámnak számnak kell lennie!");
+        }
+        adatok = honapok[honapinput -1]
+        output.textContent = `${honapinput} Hónap: ${adatok.nev} Évszak: ${adatok.evszak} Napok száma: ${adatok.napok}`;
+    }
+    catch (error) {
+        output.textContent = `${honapinput} Hiba: ${error.message}`
+    }
 }
