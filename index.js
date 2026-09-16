b8c5a44e5838ebf604e6185f27cc55248671f0ea
MartinCsupor <csupormartin@turr.hu>
2026-09-16 11:05:02 +0200

    1. honapok tömb

---
+++ index.js
@@ -0,0 +1,49 @@
+const honapok = [{
+    nev: "Január",
+    evszak: "Tél",
+    napok: 31
+}, {
+    nev: "Február",
+    evszak: "Tél",
+    napok: 28
+}, {
+    nev: "Március",
+    evszak: "Tavasz",
+    napok: 31
+}, {
+    nev: "Április",
+    evszak: "Tavasz",
+    napok: 30
+}, {
+    nev: "Május",
+    evszak: "Tavasz",
+    napok: 31
+}, {
+    nev: "Június",
+    evszak: "Nyár",
+    napok: 30
+}, {
+    nev: "Július",
+    evszak: "Nyár",
+    napok: 31
+}, {
+    nev: "Augusztus",
+    evszak: "Nyár",
+    napok: 31
+}, {
+    nev: "Szeptember",
+    evszak: "Ősz",
+    napok: 30
+}, {
+    nev: "Október",
+    evszak: "Ősz",
+    napok: 31
+}, {
+    nev: "November",
+    evszak: "Ősz",
+    napok: 30
+}, {
+    nev: "December",
+    evszak: "Tél",
+    napok: 31
+}]
